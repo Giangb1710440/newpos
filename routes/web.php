37ad3c944fe52d116495bce75c87ccd79835ad40
b1710440 <giangb1710440@student.ctu.edu.vn>
2021-06-21 22:48:56 +0700

    lan 1

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('home','App\Http\Controllers\HomeController@index')->name('home');
+Route::get('/login','App\Http\Controllers\HomeController@login')->name('login');
+Route::get('/optionproduct','App\Http\Controllers\HomeController@optionproduct')->name('optionproduct');
+Route::get('/addproduct','App\Http\Controllers\HomeController@addproduct')->name('addproduct');
+Route::get('/addcustomer','App\Http\Controllers\HomeController@themkh')->name('themkh');
