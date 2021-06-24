30de32e69b01a6877c7c4ba2722dc8cdccda580a
tbminh <minhbao24101999@gmail.com>
2021-06-24 09:12:23 +0700

    lam lai

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -4,42 +4,7 @@
 @section('content')
 
     <div class="right_col" role="main">
-                <!-- top tiles -->
-            {{--            <div class="row" style="display: inline-block;" >--}}
-            {{--                <div class="tile_count">--}}
-            {{--                    <div class="col-md-2 col-sm-4  tile_stats_count">--}}
-            {{--                        <span class="count_top"><i class="fa fa-user"></i> Total Users</span>--}}
-            {{--                        <div class="count">2500</div>--}}
-            {{--                        <span class="count_bottom"><i class="green">4% </i> From last Week</span>--}}
-            {{--                    </div>--}}
-            {{--                    <div class="col-md-2 col-sm-4  tile_stats_count">--}}
-            {{--                        <span class="count_top"><i class="fa fa-clock-o"></i> Average Time</span>--}}
-            {{--                        <div class="count">123.50</div>--}}
-            {{--                        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>3% </i> From last Week</span>--}}
-            {{--                    </div>--}}
-            {{--                    <div class="col-md-2 col-sm-4  tile_stats_count">--}}
-            {{--                        <span class="count_top"><i class="fa fa-user"></i> Total Males</span>--}}
-            {{--                        <div class="count green">2,500</div>--}}
-            {{--                        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>--}}
-            {{--                    </div>--}}
-            {{--                    <div class="col-md-2 col-sm-4  tile_stats_count">--}}
-            {{--                        <span class="count_top"><i class="fa fa-user"></i> Total Females</span>--}}
-            {{--                        <div class="count">4,567</div>--}}
-            {{--                        <span class="count_bottom"><i class="red"><i class="fa fa-sort-desc"></i>12% </i> From last Week</span>--}}
-            {{--                    </div>--}}
-            {{--                    <div class="col-md-2 col-sm-4  tile_stats_count">--}}
-            {{--                        <span class="count_top"><i class="fa fa-user"></i> Total Collections</span>--}}
-            {{--                        <div class="count">2,315</div>--}}
-            {{--                        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>--}}
-            {{--                    </div>--}}
-            {{--                    <div class="col-md-2 col-sm-4  tile_stats_count">--}}
-            {{--                        <span class="count_top"><i class="fa fa-user"></i> Total Connections</span>--}}
-            {{--                        <div class="count">7,325</div>--}}
-            {{--                        <span class="count_bottom"><i class="green"><i class="fa fa-sort-asc"></i>34% </i> From last Week</span>--}}
-            {{--                    </div>--}}
-            {{--                </div>--}}
-            {{--            </div>--}}
-            <!-- /top tiles -->
+                
 
                 <div class="row">
                     <div class="col-md-12 col-sm-12 ">
@@ -47,7 +12,7 @@
 
                             <div class="row x_title">
                                 <div class="col-md-6">
-                                    <h3>Biểu đồ </h3>
+                                    <h3>Biểu đồ doanh thu hàng tháng</h3>
                                 </div>
                                 <div class="col-md-6">
                                     <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
@@ -62,7 +27,11 @@
                             </div>
                             <div class="col-md-3 col-sm-3  bg-white">
                                 <div class="x_title">
-                                    <h2>Ngành hàng bán chạy</h2>
+                                    <h2>Ngành hàng bán chạy nhất</h2>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="x_title">
+                                    <h2>Ngành hàng bán chạy thứ hai</h2>
                                     <div class="clearfix"></div>
                                 </div>
 
